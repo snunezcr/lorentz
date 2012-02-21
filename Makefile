5502a52f83213bcbb81bdbc21e786c4f65fa297d
Santiago Nunez-Corrales <snunezcr@gmail.com>
2012-02-21 06:40:25 -0600

    Adding stochastic version of the Lorenz equations

--- Makefile
+++ Makefile
@@ -2,10 +2,13 @@ CC=gcc
 CFLAGS=-O2 -I.
 LDFLAGS=-lm
 
-all: lorenz exp2text statistics stat2text
+all: lorenz slorenz exp2text statistics stat2text
 
 lorenz: lorenz.c
 	$(CC) $(CFLAGS) $(@).c -o $(@) $(LDFLAGS)
+
+slorenz: slorenz.c
+	$(CC) $(CFLAGS) $(@).c ranlib.c linpack.c com.c -o $(@) $(LDFLAGS)
 
 exp2text: exp2text.c
 	$(CC) $(CFLAGS) $(@).c -o $(@) $(LDFLAGS)
@@ -18,6 +21,7 @@ stat2text: stat2text.c
 
 clean:
 	rm -f lorenz
+	rm -f slorenz
 	rm -f exp2text
 	rm -f statistics
 	rm -f stat2text
---
+++ slorenz.c
@@ -0,0 +1,103 @@
+/*
+ * Deterministic Lorenz equations code
+ * Research on sensitivity to initial conditions
+ *
+ * The code receives a set of coordinates of origin and
+ * a vector of spherical coordinates in order to cover a
+ * sphere of points around the origin.
+ *
+ * Santiago Nunez Corrales
+ * Eric Jakobsson
+ * 2012
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <expfile.h>
+
+#define R 0
+#define PHI 1
+#define PSI 2
+
+#define PI 3.141592654
+
+int main (int argc, char *argv[]) {
+	int i;
+	double curr[DIM];
+	double next[DIM];
+	double tran[DIM];
+	double sphr[DIM];
+	double h, a, b, c;
+	double sigma, dw;
+	FILE *output;
+	struct exp_header header;
+	
+	h = 0.01;
+	a = 10.0;
+	b = 28.0;
+	c = 8.0/3.0;
+
+	if (argc != 8) {
+		fprintf(stderr, "Number of parameters incorrect: %d.\n", argc);
+		return -1;
+	}
+
+	curr[X] = atof(argv[1]);
+	curr[Y] = atof(argv[2]);
+	curr[Z] = atof(argv[3]);
+
+	sphr[R] = atof(argv[4]);
+	sphr[PHI] = atof(argv[5]);
+	sphr[PSI] = atof(argv[6]);
+
+	sigma = atof(argv[7]);
+
+	header.count = N;
+	memcpy(header.id, "LRNZE", FID*sizeof(char));
+	header.x = curr[X];
+	header.y = curr[Y];
+	header.z = curr[Z];
+	header.r = sphr[R];
+	header.phi = sphr[PHI];
+	header.psi = sphr[PSI];
+
+	sphr[PHI] *= PI/180.0;
+	sphr[PSI] *= PI/180.0;
+
+	tran[X] = sphr[R]*cos(sphr[PHI])*sin(sphr[PSI]);
+	tran[Y] = sphr[R]*sin(sphr[PHI])*cos(sphr[PSI]);
+	tran[Z] = sphr[R]*cos(sphr[PSI]);
+
+	curr[X] += tran[X];
+	curr[Y] += tran[Y];
+	curr[Z] += tran[Z];
+
+	if ( (output = fopen(argv[8], "wb")) == NULL) {
+		fprintf(stderr, "Results file creation failed.\n");
+		fclose(output);
+		return -1;
+	}
+
+	fwrite(&header, 1, sizeof(header), output);
+	
+	for (i = 0; i < N; i++) {
+		dw = sqrt(h)*gennor(0,1);
+
+		next[X] = curr[X] + h*a*(curr[Y] - curr[X]) + sigma*curr[X]*dw;
+		next[Y] = curr[Y] + h*(curr[X]*(b - curr[Z]) - curr[Y]) + sigma*curr[Y]*dw;
+		next[Z] = curr[Z] + h*(curr[X]*curr[Y] - c*curr[Z]) + sigma*curr[Z]*dw;
+
+		curr[X] = next[X];
+		curr[Y] = next[Y];
+		curr[Z] = next[Z];
+
+		fwrite(curr, DIM, sizeof(double), output);
+	}
+
+	fclose(output);
+
+	return 0;
+}
+
