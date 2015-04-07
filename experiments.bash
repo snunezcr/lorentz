71a6947d9ac8cd4f05444733f32df5c3b937d503
Santiago Nunez-Corrales <snunezcr@gmail.com>
2015-04-06 19:42:53 -0600

    Bash file for running experiments

---
+++ experiments.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+./tlorenz 2 2 2 10 10 1 data-10-1.0.txt
+./tlorenz 2 2 2 10 10 0.5 data-10-0.5.txt
+./tlorenz 2 2 2 10 10 0.25 data-10-0.25.txt
+./tlorenz 2 2 2 10 10 0.125 data-10-0.125.txt
+./tlorenz 2 2 2 10 10 0.0625 data-10-0.0625.txt
+
+./tlorenz 2 2 2 25 10 1 data-25-1.0.txt
+./tlorenz 2 2 2 25 10 0.5 data-25-0.5.txt
+./tlorenz 2 2 2 25 10 0.25 data-25-0.25.txt
+./tlorenz 2 2 2 25 10 0.125 data-25-0.125.txt
+./tlorenz 2 2 2 25 10 0.0625 data-25-0.0625.txt
+
+./tlorenz 2 2 2 50 10 1 data-50-1.0.txt
+./tlorenz 2 2 2 50 10 0.5 data-50-0.5.txt
+./tlorenz 2 2 2 50 10 0.25 data-50-0.25.txt
+./tlorenz 2 2 2 50 10 0.125 data-50-0.125.txt
+./tlorenz 2 2 2 50 10 0.0625 data-50-0.0625.txt
+
+./tlorenz 2 2 2 100 10 1 data-100-1.0.txt
+./tlorenz 2 2 2 100 10 0.5 data-100-0.5.txt
+./tlorenz 2 2 2 100 10 0.25 data-100-0.25.txt
+./tlorenz 2 2 2 100 10 0.125 data-100-0.125.txt
+./tlorenz 2 2 2 100 10 0.0625 data-100-0.0625.txt
